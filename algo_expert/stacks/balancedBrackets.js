ff4c943359d0a640c8768448d0c6fdc8dc1d6101
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-07-17 22:47:30 +0200

    stack balanced brackets

---
+++ algo_expert/stacks/balancedBrackets.js
@@ -0,0 +1,51 @@
+
+
+const closingBrackets = [')', ']', '}'];
+const openingBrackets = ['(', '[', '{'];
+const bracketPairs = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+}
+
+
+function balancedBrackets(string) {
+    let stack = [];
+    for (let i=0; i<string.length; i++){
+        ch = string[i];
+        // finding closing bracket
+        if (closingBrackets.includes(ch))
+        {
+            lastStackElement = stack.pop();
+            if (bracketPairs[lastStackElement] !== ch)
+            {
+                return false
+            }
+        } else if (openingBrackets.includes(ch)){
+            stack.push(ch);
+        }
+    } 
+    if (stack.length > 0){
+        console.log(`The stack is not empty ${stack} and has : ${stack.length} elements.`);
+        return false
+    }
+    return true
+        
+  }
+  
+
+
+
+// let text = "([])(){}(())()()"
+
+// console.log(`The input text ${text} is balanced: ${balancedBrackets(text)}`);
+
+
+text2 = "()[]{}{"
+
+console.log(`\n The input text2 ${text2} is balanced: ${balancedBrackets(text2)}`);
+
+
+text2 = "(a)"
+
+console.log(`\n The input text2 ${text2} is balanced: ${balancedBrackets(text2)}`);
