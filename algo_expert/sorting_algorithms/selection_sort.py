a2755abe88c9c53872916e1b56314bab0d017363
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-07-18 10:46:32 +0200

    python selection sort

---
+++ algo_expert/sorting_algorithms/selection_sort.py
@@ -0,0 +1,33 @@
+from typing import List, Union
+
+
+
+def swap(array, i, j):
+    array[i], array[j] = array[j], array[i]
+
+
+def findMinEl(array: List, starting_index: int) -> List[Union[int, float]]:
+    minValue = float("+inf")
+    minIdx = 0
+    for j in range(starting_index, len(array)):
+        if minValue > array[j]:
+            minValue = array[j]
+            minIdx = j
+    return minIdx
+
+
+def selectionSort(array):
+    for i in range(len(array)):
+        minIdx = findMinEl(array, i)
+        swap(array, i, minIdx)
+    return array
+
+
+input_arr = [4, 54, 1, 0, -4, 9]
+print(f"Input array goes as follows {input_arr}")
+print(f"Sorted array {selectionSort(input_arr)}")
+
+
+input_arr = [2, 1]
+print(f"\n\n Input array goes as follows {input_arr}")
+print(f"Sorted array {selectionSort(input_arr)}")
