d5249e36b3103ceb3ebe80fd31e262b495986dde
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-07-27 10:21:36 +0200

    iterative solution with stack

--- leet_code/234_palidrome.py
+++ leet_code/234_palidrome.py
@@ -18,22 +18,40 @@ class ListNode:
 
 
 #  TC O(2n) SC O(n)
+# class Solution:
+#     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        
+#         arr = []
+#         while head is not None:
+#             arr.append(head.val)
+#             head = head.next
+        
+#         mid = len(arr) // 2
+#         for i in range(mid):
+#             el1 = arr[i]
+#             el2 = arr[-i-1]
+#             if el1 != el2:
+#                 return False
+#         return True
+
+
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         
-        arr = []
-        while head is not None:
-            arr.append(head.val)
+        s = list()
+        curr = head
+        while curr is not None:
+            s.append(curr.val)
+            curr = curr.next
+
+        while len(s) > 0:
+            el1 = s.pop()
+            el2 = head.val
             head = head.next
-        
-        mid = len(arr) // 2
-        for i in range(mid):
-            el1 = arr[i]
-            el2 = arr[-i-1]
             if el1 != el2:
                 return False
         return True
-
 
 
 # Creating the list [1, 2, 2, 1]
@@ -48,3 +66,16 @@ print_list(head)
 s = Solution()
 is_palidrome = s.isPalindrome(head)
 print(f"isPalindrome: {is_palidrome}")
+
+
+
+node22 = ListNode(2)
+head2 = ListNode(1, node22) # This is the head of the list
+
+print("\n\ninitial list: ", end="")
+print_list(head2)
+s = Solution()
+is_palidrome2 = s.isPalindrome(head2)
+print(f"isPalindrome2: {is_palidrome2}")
+
+
