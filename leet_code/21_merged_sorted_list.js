e13603fe1d974f455fad772d0fb7ba190d2ef986
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-07-27 06:53:07 +0200

    recursive java scrpit

---
+++ leet_code/21_merged_sorted_list.js
@@ -0,0 +1,60 @@
+
+
+class ListNode {
+    constructor(val=0, next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+function printList(node) {
+    let currentNode = node;
+    while(currentNode !== null) {
+        process.stdout.write(currentNode.val + " ");
+        currentNode = currentNode.next;
+    }
+    console.log();
+}
+
+
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    
+    if (list1 === null){
+        return list2;
+    } else if(list2===null){
+        return list1;
+    } else if (list1.val < list2.val){
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    } else{
+        list2.next =  mergeTwoLists(list1, list2.next);
+        return list2
+    }
+};
+
+// Creating the first list [1, 2, 4]
+let list1_node1 = new ListNode(4);
+let list1_node2 = new ListNode(2, list1_node1);
+let list1 = new ListNode(1, list1_node2); // This is the head of the first list
+
+// Creating the second list [1, 3, 4]
+let list2_node1 = new ListNode(4);
+let list2_node2 = new ListNode(3, list2_node1);
+let list2 = new ListNode(1, list2_node2); // This is the head of the second list
+
+// Now let's print our lists
+process.stdout.write("List 1: ");
+printList(list1);
+
+process.stdout.write("List 2: ");
+printList(list2);
+
+
+let res = mergeTwoLists(list1, list2);
+console.log(`mergeTwoLists`);
+printList(res);
--- leet_code/21_merged_sorted_list.py
+++ leet_code/21_merged_sorted_list.py
@@ -68,6 +68,13 @@ def print_list(node):
 
 
 
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 
