0ec67b7ac31e8015c9d91beb86e8b3c3eb31f078
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-07-29 10:49:34 +0200

    small refactorization

--- leet_code/669_trim_bst.py
+++ leet_code/669_trim_bst.py
@@ -61,9 +61,9 @@ class Solution:
             return self.trimBST(root.right, low, high)
         elif root.val > high:
             return self.trimBST(root.left, low, high)
-        else:
-            root.left = self.trimBST(root.left, low, high)
-            root.right = self.trimBST(root.right, low, high)
+        
+        root.left = self.trimBST(root.left, low, high)
+        root.right = self.trimBST(root.right, low, high)
         return root
 
 
