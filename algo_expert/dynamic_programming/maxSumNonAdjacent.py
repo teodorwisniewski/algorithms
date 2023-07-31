835471d243f3e50d3ef08e328a7390769b505542
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-07-31 10:13:32 +0200

    more optimal solution

--- algo_expert/dynamic_programming/maxSumNonAdjacent.py
+++ algo_expert/dynamic_programming/maxSumNonAdjacent.py
@@ -1,24 +1,45 @@
 
 
 
-# TC O(n) SC O(n)
+# # TC O(n) SC O(n)
+# def maxSubsetSumNoAdjacent(array):
+#     if len(array) == 0:
+#         return 0
+#     if len(array) < 3:
+#         return max(array)
+    
+#     sum_array = [0 for _ in array]
+#     sum_array[0] = array[0]
+#     sum_array[1] = max(array[0], array[1])
+    
+#     for i in range(2, len(array)):
+#         second_el = sum_array[i-2] + array[i]
+#         sum_array[i] = max(sum_array[i-1], second_el)
+
+#     return sum_array[-1]
+    
+
+
+
+# TC O(n) SC O(1)
 def maxSubsetSumNoAdjacent(array):
     if len(array) == 0:
         return 0
     if len(array) < 3:
         return max(array)
     
-    sum_array = [0 for _ in array]
-    sum_array[0] = array[0]
-    sum_array[1] = max(array[0], array[1])
+    sum_array = []
+    sum_array.append(array[0])
+    sum_array.append(max(array[0], array[1]))
     
     for i in range(2, len(array)):
-        second_el = sum_array[i-2] + array[i]
-        sum_array[i] = max(sum_array[i-1], second_el)
+        first_el = sum_array[1]
+        second_el = sum_array[0] + array[i]
+        current_max = max(first_el, second_el)
+        sum_array[0], sum_array[1] = sum_array[1], current_max
 
-    return sum_array[-1]
+    return current_max
     
-
 
 
 
