66db2b1267547847eaabaa7590be96241d3b2771
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-08-02 09:42:49 +0200

    ways to make change

---
+++ algo_expert/dynamic_programming/ways_to_make_change.py
@@ -0,0 +1,20 @@
+
+
+
+
+def numberOfWaysToMakeChange(n, denoms):
+    # Write your code here.
+    ways = [0 for _ in range(n+1)]
+    ways[0] = 1
+    for denom in denoms:
+        for amount in range(1, n+1):
+            if denom <= amount:
+                ways[amount] += ways[amount-denom]
+    return ways[-1]
+
+
+denoms = [1, 5, 10, 25]
+target = 10
+res = numberOfWaysToMakeChange(target, denoms)
+print(f"donoms {denoms} target={target}")
+print(f"numberOfWaysToMakeChange {res}")
