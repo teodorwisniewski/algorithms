a07a076eba96e99f4ed2e2d550a87a317acf0f23
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-08-04 12:54:21 +0200

    blackjackProbability

---
+++ algo_expert/recursion/blackjack_probability.py
@@ -0,0 +1,43 @@
+
+
+
+
+#  TC O(t-s) SC O(t-s)
+def blackjackProbability(target, startingHand):
+    memo = {}
+    return rec_helper(target, startingHand, memo)
+
+
+def rec_helper(target, current_hand, memo):
+
+    if startingHand > target:
+        return 0.1
+    if  (target - 4) <= startingHand <= target:
+        return 0
+    if startingHand in memo:
+        return memo[startingHand]
+    
+    prob = 0.0 
+    for card_nb in range(1, 11):
+        new_startingHand = current_hand + card_nb
+        if new_startingHand < target - 4:
+            prob += 0.1 * rec_helper(target, new_startingHand, memo)
+        elif new_startingHand > target:
+            prob += 0.1
+    memo[startingHand] = round(prob, 3)
+    return memo[startingHand]
+
+
+
+target = 21
+startingHand = 15
+res = blackjackProbability(target, startingHand)
+print(f"taget={target} startingHand={startingHand}")
+print(f"blackjackProbability {res}")
+
+
+target = 100
+startingHand = 90
+res = blackjackProbability(target, startingHand)
+print(f"taget={target} startingHand={startingHand}")
+print(f"blackjackProbability {res}")
