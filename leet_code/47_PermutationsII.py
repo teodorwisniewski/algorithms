c68a80d40b01538e303f8f1a382f9b1864b35f54
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-08-17 10:56:48 +0200

    most efficient TC

--- leet_code/47_PermutationsII.py
+++ leet_code/47_PermutationsII.py
@@ -1,31 +1,57 @@
 from typing import List
+from collections import Counter
+
+
+# class Solution:
+#     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+
+#         def backtrack(perm, idx, numbers):
+
+#             if perm in res:
+#                 return
+
+#             if len(perm) == len(nums):
+#                 res.append(perm.copy())
+#                 return
+#             prev = None
+#             for i in range(len(numbers)):
+#                 num = numbers[i]
+#                 if prev == num:
+#                     continue
+#                 nums_without = numbers[:i] + numbers[i+1:]
+#                 perm.append(num)
+#                 backtrack(perm, i+1, nums_without)
+#                 perm.pop()
+#                 prev = num
+    
+#         res = []
+#         nums.sort()
+#         backtrack([], 0, nums)
+#         return res
+
 
 
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
 
-        def backtrack(perm, idx, numbers):
-
-            if perm in res:
-                return
+        def backtrack():
 
             if len(perm) == len(nums):
                 res.append(perm.copy())
                 return
-            prev = None
-            for i in range(len(numbers)):
-                num = numbers[i]
-                if prev == num:
+            for num in counted_nums:
+                if not counted_nums[num]:
                     continue
-                nums_without = numbers[:i] + numbers[i+1:]
                 perm.append(num)
-                backtrack(perm, i+1, nums_without)
+                counted_nums[num] -= 1
+                backtrack()
                 perm.pop()
-                prev = num
+                counted_nums[num] += 1
     
         res = []
-        nums.sort()
-        backtrack([], 0, nums)
+        counted_nums = Counter(nums)
+        perm = []
+        backtrack()
         return res
 
 
