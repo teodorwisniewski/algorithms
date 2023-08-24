f401aef28df0bd7d49b69a7415ed7a3d8ccb2293
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-08-24 22:19:41 +0200

    remove invalid parethesis

---
+++ leet_code/301_Remove Invalid_Parentheses.py
@@ -0,0 +1,58 @@
+
+from typing import List
+
+
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        self.string = s
+        self.res = set()
+        self.longest_str = -1
+        self.backtrack(0, [], 0, 0)
+        return self.res
+
+    def backtrack(self, curr_idx: int, curr_res: List, left_count: int,
+                  right_count: int):
+
+        if curr_idx >= len(self.string):
+            if left_count == right_count:
+                if len(curr_res) > self.longest_str:
+                    self.longest_str = len(curr_res)
+                    self.res = set()
+                    self.res.add("".join(curr_res))
+                elif len(curr_res) == self.longest_str:
+                    self.res.add("".join(curr_res))
+        else:
+            curr_char = self.string[curr_idx]
+            if curr_char == "(":
+                curr_res.append(curr_char)
+                self.backtrack(curr_idx+1, curr_res, left_count+1, right_count)
+                curr_res.pop()
+                self.backtrack(curr_idx+1, curr_res, left_count, right_count)
+            elif curr_char == ")":
+                if left_count > right_count:
+                    curr_res.append(curr_char)
+                    self.backtrack(curr_idx+1, curr_res, left_count,
+                                   right_count+1)
+                    curr_res.pop()
+                self.backtrack(curr_idx+1, curr_res, left_count, right_count)
+            else:
+                curr_res.append(curr_char)
+                self.backtrack(curr_idx+1, curr_res, left_count, right_count)
+                curr_res.pop()
+
+
+
+
+
+
+
+sol = Solution()
+
+# s = "()())()"
+# res = sol.removeInvalidParentheses(s)
+# print(f"removeInvalidParentheses {res}")
+
+s2 = ")(f"
+res = sol.removeInvalidParentheses(s2)
+print(f"removeInvalidParentheses {res}")
+
