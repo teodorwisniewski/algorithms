a8883de5f6e707101d24e5db20aab414838e8b45
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-08-31 09:56:46 +0200

    memoziation

---
+++ algo_expert/recursion/nb_of_binary_tree_topologies.py
@@ -0,0 +1,26 @@
+
+def numberOfBinaryTreeTopologies(n, dp=None):
+    if dp is None:
+        dp = {}
+    if n in dp:
+        return dp[n]
+    if n == 0:
+        return 1
+    total_nb_of_trees = 0
+    for l in range(n):
+        r = n - 1 - l
+        nb_left = numberOfBinaryTreeTopologies(l)
+        nb_right = numberOfBinaryTreeTopologies(r)
+        total_nb_of_trees += nb_left * nb_right
+    dp[n] = total_nb_of_trees
+    return total_nb_of_trees
+
+
+n = 3
+res = numberOfBinaryTreeTopologies(n)
+print(f"numberOfBinaryTreeTopologies {res}")
+
+
+n = 4
+res = numberOfBinaryTreeTopologies(n)
+print(f"numberOfBinaryTreeTopologies {res}")
