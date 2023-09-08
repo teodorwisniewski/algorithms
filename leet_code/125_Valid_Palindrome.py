7dfce0981b8398141ab11d3dd84ba99597d202cd
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-09-08 11:05:25 +0200

    asci is alpha nummeric

--- leet_code/125_Valid_Palindrome.py
+++ leet_code/125_Valid_Palindrome.py
@@ -2,11 +2,41 @@
 
 
 
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        s = "".join([char for char in s if char.isalnum()])
-        return s == s[::-1]
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         s = s.lower()
+#         s = "".join([char for char in s if char.isalnum()])
+#         return s == s[::-1]
+
+
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+    
+#         left, right = 0, len(s) - 1
+
+#         while left < right:
+#             if not self.is_alpha_nummeric(s[left]):
+#                 left += 1
+#             elif not self.is_alpha_nummeric(s[right]):
+#                 right -= 1
+#             elif s[left].lower() != s[right].lower():
+#                 return False
+#             else:
+#                 left += 1
+#                 right -= 1
+
+#         return True
+    
+#     def is_alpha_nummeric(self, ch: str) -> bool:
+#         cond = (
+#             ord("A") <= ord(ch) <= ord("Z") or
+#             ord("a") <= ord(ch) <= ord("z") or
+#             ord("0") <= ord(ch) <= ord("9")
+#         )
+#         if cond:
+#             return True
+#         return False
+
 
 
 input = "A man, a plan, a canal: Panama"
