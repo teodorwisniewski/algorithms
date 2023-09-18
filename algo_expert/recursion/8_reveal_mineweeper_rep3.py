60688ffac17ed06828a6c2207e5b795a739d727b
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-09-18 09:05:46 +0200

    reveeal mines weeper

---
+++ algo_expert/recursion/8_reveal_mineweeper_rep3.py
@@ -0,0 +1,96 @@
+
+def revealMinesweeper(board, row, column):
+    if board[row][column] == 'M':
+        board[row][column] = "X"
+        return board
+    return solve_partial_mines_weeper(board, row, column)
+
+
+def solve_partial_mines_weeper(board, row, column):
+
+    neighbors = get_neighbors(board, row, column)
+    nb_adjacent_mines = 0
+    for neig_row, neig_column in neighbors:
+        if board[neig_row][neig_column] == "M":
+            nb_adjacent_mines += 1
+
+    if nb_adjacent_mines:
+        board[row][column] = str(nb_adjacent_mines)
+        return board
+
+    board[row][column] = "0"
+    for neig_row, neig_column in neighbors:
+        if board[neig_row][neig_column] == "H":
+            solve_partial_mines_weeper(board, neig_row, neig_column)
+
+    return board
+
+
+def get_neighbors(board, row, column):
+
+    deltas = [-1, 0, 1]
+    potential_steps = [
+        (delta_x, delta_y)
+        for delta_y in deltas
+        for delta_x in deltas
+        if not (delta_x == 0 and delta_y == 0)
+    ]
+    neighbors = []
+    for delta_x, delta_y in potential_steps:
+        new_row = row + delta_x
+        new_col = column + delta_y
+        if 0 <= new_row < len(board) and 0 <= new_col < len(board[0]):
+            neighbors.append((new_row, new_col))
+    return neighbors
+        
+    
+    
+
+    
+
+
+
+
+
+board = [
+  ["M", "M"],
+  ["H", "H"],
+  ["H", "H"]
+] 
+
+row_nb = 2
+col_nb = 0
+
+
+# # Print the board
+# for row in board:
+#     print(' '.join(row))
+
+
+# Print the word
+# print("row:", row_nb, " col:", col_nb)
+# output_board = revealMinesweeper(board, row_nb, col_nb)
+
+# print("revealMinesweeper output")
+# for row in output_board:
+#     print(' '.join(row))
+
+expected_output = [
+  ["M", "M"],
+  ["2", "2"],
+  ["0", "0"]
+]
+
+# assert output_board == expected_output
+
+board = [
+    ["H", "M"]
+  ]
+row_nb = 0
+col_nb = 0
+
+output_board = revealMinesweeper(board, row_nb, col_nb)
+
+print("revealMinesweeper output")
+for row in output_board:
+    print(' '.join(row))
