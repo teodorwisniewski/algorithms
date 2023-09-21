20a8817e8ca8a207fe8ce8000be8807c452b6411
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-09-21 10:32:01 +0200

    word search

---
+++ leet_code/79_word_search_rep1.py
@@ -0,0 +1,74 @@
+
+from typing import List
+
+
+# TC O(row*col*4^len(word)) SC(len(word))
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+
+        ROWS, COLS = len(board), len(board[0])
+        n = len(word)
+        path = set()
+
+        def dfs(row, col, idx):
+
+            if idx == n:
+                return True
+
+            cond = (
+                row < 0 or 
+                col < 0 or
+                row >= ROWS or
+                col >= COLS or
+                word[idx] != board[row][col] or
+                (row, col) in path
+            )    
+            if cond:
+                return False
+            
+            path.add((row, col))
+            res = (
+                dfs(row+1, col, idx+1) or
+                dfs(row-1, col, idx+1) or
+                dfs(row, col-1, idx+1) or
+                dfs(row, col+1, idx+1) 
+            )
+            path.remove((row, col))
+            return res
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0):
+                    return True
+        return False
+            
+
+   
+board = [
+    list("SIEE"),
+    list("SFMS"),
+    list("ONAE")
+]
+word = "SIEMANO"
+
+s = Solution()
+
+# Print the board
+for row in board:
+    print(' '.join(row))
+
+
+word = "SIEMANO"
+# Print the word
+print("Word:", word)
+print(f"word_exist {s.exist(board, word)}")
+
+word = "SIEMA"
+# Print the word
+print("Word:", word)
+print(f"word_exist {s.exist(board, word)}")
+
+word = "SIEMU"
+# Print the word
+print("Word:", word)
+print(f"word_exist {s.exist(board, word)}")
