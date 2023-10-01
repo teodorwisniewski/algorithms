05c010f0c613240cb8c66eb4162b5f0ce178ab72
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-10-01 15:03:30 +0200

    productSum

---
+++ algo_expert/recursion/2_productSum_rep2.py
@@ -0,0 +1,17 @@
+
+
+
+def productSum(array, multiplier=1):
+    total_sum = 0
+    for el in array:
+        if isinstance(el, list):
+            total_sum += productSum(el, multiplier+1)
+        else:
+            total_sum += el
+    return total_sum * multiplier
+
+
+
+arr  = [5, 2, [7, -1], 3, [6, [-13, 8], 4]]
+res = productSum(arr)
+print(f'productSum {res}')
