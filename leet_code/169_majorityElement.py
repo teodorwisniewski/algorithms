962dd47c477aa6a548d3e273277b4285085b540a
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-10-02 10:26:58 +0200

    Boyer - Moore

--- leet_code/169_majorityElement.py
+++ leet_code/169_majorityElement.py
@@ -2,15 +2,27 @@ from typing import List
 
 
 
-# TC O(n) SC O(n)
+# # TC O(n) SC O(n)
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         counter = {}
+#         max_count, res = 0, 0
+#         for num in nums:
+#             counter[num] = 1 + counter.get(num, 0)
+#             res = num if counter[num] > max_count else res
+#             max_count = max(max_count, counter[num])
+#         return res
+
+
+# TC O(n) SC O(1)
+# Boyer - Moore solution
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counter = {}
-        max_count, res = 0, 0
+        res, count = 0, 0
         for num in nums:
-            counter[num] = 1 + counter.get(num, 0)
-            res = num if counter[num] > max_count else res
-            max_count = max(max_count, counter[num])
+            if count == 0:
+                res = num
+            count += 1 if num == res else -1
         return res
 
 
