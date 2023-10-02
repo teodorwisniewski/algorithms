5b50a95c0ba44a793d0272156c6bad93453e843e
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-10-02 10:25:10 +0200

    first solution

---
+++ leet_code/169_majorityElement.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+
+# TC O(n) SC O(n)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        counter = {}
+        max_count, res = 0, 0
+        for num in nums:
+            counter[num] = 1 + counter.get(num, 0)
+            res = num if counter[num] > max_count else res
+            max_count = max(max_count, counter[num])
+        return res
+
+
+
+sol = Solution()
+nums = [2,2,1,1,1,2,2]
+res = sol.majorityElement(nums)
+print(f"majorityElement {res}")
