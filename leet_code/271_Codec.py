b4a8d381beef3cef195cc0016cf378d4c108e160
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-10-20 09:34:07 +0200

    Encode and Decode Strings

---
+++ leet_code/271_Codec.py
@@ -0,0 +1,40 @@
+from typing import List
+
+
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """
+        Encodes a list of strings to a single string.
+        """
+        encoded_str = ""
+        for word in strs:
+            encoded_str += str(len(word)) + "#" + word
+        return encoded_str
+        
+    def decode(self, s: str) -> List[str]:
+        """
+        Decodes a single string to a list of strings.
+        """
+        decoded_strs = []
+        idx = 0
+        while idx < len(s):
+            j = idx
+            while s[j] != "#":
+                j += 1
+            word_len = int(s[idx:j])
+            word = s[j+1:j+1+word_len]
+            decoded_strs.append(word)
+            idx = j + 1 + word_len
+            
+        return decoded_strs
+
+
+
+
+
+codec = Codec()
+dummy_input = ["Hello","World"]
+encoded_str = codec.encode(dummy_input)
+print(f"encoded_str {encoded_str}")
+dencoded_str = codec.decode(encoded_str)
+print(f"dencoded_str {dencoded_str}")
