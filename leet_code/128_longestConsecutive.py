e2521b7d6b07b4f30ee41eba5139abdfbf0a362f
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-10-21 13:20:45 +0200

    more efficient solution

--- leet_code/128_longestConsecutive.py
+++ leet_code/128_longestConsecutive.py
@@ -1,25 +1,65 @@
 from typing import List
 
 
+# class Solution:
+#     def longestConsecutive(self, nums: List[int]) -> int:
+
+#         nums_set = set(nums)
+#         longest = 0
+
+#         for num in nums:
+#             if (num - 1) not in nums_set:
+#                 length = 1
+#                 while (num + length) in nums_set:
+#                     length += 1
+#                 longest = max(longest, length)
+#         return longest
+                
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
 
         nums_set = set(nums)
         longest = 0
 
-        for num in nums:
+        while nums_set:
 
-            if (num - 1) not in nums_set:
+            num = nums_set.pop()
+            curr_len = 1
 
-                length = 1
-                while (num + length) in nums_set:
-                    length += 1
-                longest = max(longest, length)
+            l = num - 1
+            r = num + 1
+
+            if l in nums_set and r in nums_set:
+
+                while r in nums_set:
+                    nums_set.remove(r)
+                    r += 1
+                    curr_len += 1
+                
+                while l in nums_set:
+                    nums_set.remove(l)
+                    l -= 1
+                    curr_len += 1 
+
+            if l in nums_set:
+                
+                while l in nums_set:
+                    nums_set.remove(l)
+                    l -= 1
+                    curr_len += 1                
+ 
+            if r in nums_set:
+
+                while r in nums_set:
+                    nums_set.remove(r)
+                    r += 1
+                    curr_len += 1
+            
+            longest = max(curr_len, longest)
         return longest
                 
-
-
-
+   
 sol = Solution()
 
 nums = [100,4,200,1,3,2]
