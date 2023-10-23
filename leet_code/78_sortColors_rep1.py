a1d7a4c51287ec21200170f7845ec82dd4805929
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-10-23 08:46:54 +0200

    sort colors repetition

--- leet_code/78_sortColors_rep1.py
+++ leet_code/78_sortColors_rep1.py
@@ -4,44 +4,41 @@ from collections import Counter
 
 # class Solution:
 #     def sortColors(self, nums: List[int]) -> None:
-#         """
-#         Do not return anything, modify nums in-place instead.
-#         """
-#         buckets = Counter(nums)
+
+#         map_colors = Counter(nums)
 
 #         idx = 0
-#         for bucket in range(3):
-#             freq = buckets[bucket]
+#         for color in range(3):
+#             freq = map_colors[color]
 #             for _ in range(freq):
-#                 nums[idx] = bucket
+#                 nums[idx] = color
 #                 idx += 1
 
 
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        left, right = 0, len(nums) - 1
+
+        l, r = 0, len(nums) - 1
         idx = 0
 
         def swap(i, j):
             nums[i], nums[j] = nums[j], nums[i]
 
-        while idx <= right:
+        while idx <= r:
 
             if nums[idx] == 0:
-                swap(left, idx)
-                left += 1
-                
+                swap(l, idx)
+                l += 1
             
             if nums[idx] == 2:
-                swap(idx, right)
-                right -= 1
+                swap(r, idx)
+                r -= 1
                 idx -= 1
-
+            
             idx += 1
-        
+
+
+
 
 
 
