a3fb625d0546391c214d4480f4b316323562e9db
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-11-11 20:25:08 +0100

    push dominos

---
+++ leet_code/838_pushDominoes.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        
+        dom = list(dominoes)
+        queue = deque()
+
+        for idx, domino in enumerate(dom):
+            if domino != ".":
+                queue.append((idx, domino))
+        
+        while queue:
+            idx, domino = queue.popleft()
+            if domino == "L" and idx > 0 and dom[idx-1] == ".":
+                dom[idx-1] = "L"
+                queue.append((idx-1, "L"))
+            elif domino == "R" and idx+1 < len(dom) and dom[idx+1] == ".":
+                if idx+2 < len(dom) and dom[idx+2] == "L":
+                    queue.popleft()
+                else:
+                    dom[idx+1] = "R"
+                    queue.append((idx+1, "R"))
+        return "".join(dom)
+
+
+sol = Solution()
+# dominoes = ".L.R...LR..L.."
+# res = sol.pushDominoes(dominoes)
+# print(f"res {res}")
+
+dominoes = ".......L.L"
+res = sol.pushDominoes(dominoes)
+print(f"res {res}")
+
+
