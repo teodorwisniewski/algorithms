77f282316a4607fe876ebc1fc392692c89c426f3
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-11-29 12:41:06 +0100

    second solution

--- leet_code/1343_numOfSubarrays.py
+++ leet_code/1343_numOfSubarrays.py
@@ -1,30 +1,40 @@
 from typing import List
 
 
-class Solution:
-    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+# class Solution:
+#     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
 
-        left = 0
-        res = 0
-        window_sum = 0
-        window_size = 0
-        for num in arr:
-            if window_size == k:
-                window_sum -= arr[left]
-                left += 1
-                window_size -= 1
+#         left = 0
+#         res = 0
+#         window_sum = 0
+#         window_size = 0
+#         for num in arr:
+#             if window_size == k:
+#                 window_sum -= arr[left]
+#                 left += 1
+#                 window_size -= 1
 
-            window_size += 1
-            window_sum += num
+#             window_size += 1
+#             window_sum += num
             
-            if window_size == k and (window_sum/k) >= threshold:
-                res += 1
+#             if window_size == k and (window_sum/k) >= threshold:
+#                 res += 1
 
-        return res
+#         return res
         
 
 
-
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        res = 0
+        window_sum = sum(arr[:k-1])
+        for left in range(len(arr) - k + 1):
+            window_sum += arr[left + k - 1]
+            if (window_sum/k) >= threshold:
+                res += 1
+            window_sum -= arr[left]
+        return res
+        
 
 
 
