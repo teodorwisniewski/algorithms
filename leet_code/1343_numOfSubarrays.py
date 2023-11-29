29c5ce8bb108009f29fc397619e2e480c29b8157
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-11-29 12:16:25 +0100

    numOfSubarrays sliding window

---
+++ leet_code/1343_numOfSubarrays.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+
+        left = 0
+        res = 0
+        window_sum = 0
+        window_size = 0
+        for num in arr:
+            if window_size == k:
+                window_sum -= arr[left]
+                left += 1
+                window_size -= 1
+
+            window_size += 1
+            window_sum += num
+            
+            if window_size == k and (window_sum/k) >= threshold:
+                res += 1
+
+        return res
+        
+
+
+
+
+
+
+sol = Solution()
+arr = [2,2,2,2,5,5,5,8]
+k = 3
+threshold = 4
+
+res = sol.numOfSubarrays(arr, k, threshold)
+print(f"numOfSubarrays {res}")
