68912072f20f4a463765dc05240da602d2935e21
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-12-25 22:31:12 +0100

    altenative solution

--- leetcode/problem_1700_countStudents.py
+++ leetcode/problem_1700_countStudents.py
@@ -3,19 +3,34 @@ from collections import deque, Counter
 import pdb
 
 
+# class Solution:
+#     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:     
+#         students_cnt = Counter(students)
+#         queue = deque(students)
+#         sandwiches_stack = deque(sandwiches)
+        
+#         while sandwiches_stack and students_cnt[sandwiches_stack[0]] > 0:
+#             if sandwiches_stack[0] == queue[0]:
+#                 students_cnt[sandwiches_stack.popleft()] -= 1
+#                 queue.popleft()
+#             else:
+#                 first_person_in_queue = queue.popleft()
+#                 queue.append(first_person_in_queue)
+
+#         return len(queue)
+
+
+
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:     
         students_cnt = Counter(students)
-        queue = deque(students)
-        sandwiches_stack = deque(sandwiches)
         
-        while sandwiches_stack and students_cnt[sandwiches_stack[0]] > 0:
-            if sandwiches_stack[0] == queue[0]:
-                students_cnt[sandwiches_stack.popleft()] -= 1
-                queue.popleft()
+
+        for sandwich in sandwiches:
+
+            if students_cnt[sandwich] > 0:
+                students_cnt[sandwich] -= 1
             else:
-                first_person_in_queue = queue.popleft()
-                queue.append(first_person_in_queue)
-
-        return len(queue)
-
+                break
+        
+        return sum(students_cnt.values())
