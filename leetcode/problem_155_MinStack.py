dfe2cbb944c16e30090d38b10e84070cbf60e60b
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-12-25 11:51:26 +0100

    creating min stack

--- leetcode/problem_155_MinStack.py
+++ leetcode/problem_155_MinStack.py
@@ -1,21 +1,62 @@
+# import pdb
 
 
+# class MinStack:
 
-
-
+#     def __init__(self):
+#         self.stack = []
+#         self.correspoding_mins = []
+#         self.min_el = float('inf')
+    
+#     def push(self, val: int) -> None:
+#         self.stack.append(val)
+#         if val < self.min_el:
+#             self.min_el = val
+#         self.correspoding_mins.append(self.min_el)
+        
+#     def pop(self) -> None:
+#         if not self.stack:
+#             return None
+#         self.stack.pop()
+#         self.correspoding_mins.pop()
+#         if self.stack:
+#             self.min_el = self.correspoding_mins[-1]
+#         else:
+#             self.min_el = float('inf')
+        
+#     def top(self) -> int:
+#         if self.stack:
+# #             return self.stack[-1]
+        
+        
+#     def getMin(self) -> int:
+#         if self.stack:
+#             return self.min_el
+    
 
 class MinStack:
 
     def __init__(self):
-        
-
+        self.stack = []
+        self.correspoding_mins = []
+    
     def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.correspoding_mins and val > self.correspoding_mins[-1]:
+            new_min = self.correspoding_mins[-1]
+        else:
+            new_min = val
+        self.correspoding_mins.append(new_min)
         
-
     def pop(self) -> None:
+        self.correspoding_mins.pop()
+        self.stack.pop()
         
-
     def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
         
-
     def getMin(self) -> int:
+        if self.stack:
+            return self.correspoding_mins[-1]
+    
--- tests/test_155_min_stack.py
+++ tests/test_155_min_stack.py
@@ -11,6 +11,7 @@ def test_push():
     stack.push(7)
     assert stack.top() == 7
 
+
 def test_pop():
     stack = MinStack()
     stack.push(5)
@@ -21,10 +22,12 @@ def test_pop():
     stack.pop()
     assert stack.getMin() == 5
 
+
 def test_top():
     stack = MinStack()
     stack.push(5)
     assert stack.top() == 5
+
 
 def test_get_min():
     stack = MinStack()
@@ -34,11 +37,31 @@ def test_get_min():
     stack.push(2)
     assert stack.getMin() == 2
 
+
 def test_empty_stack():
     stack = MinStack()
-    with pytest.raises(IndexError):
-        _ = stack.top()
-    with pytest.raises(IndexError):
-        _ = stack.getMin()
+    # with pytest.raises(IndexError):
+    assert stack.top() is None
+    # with pytest.raises(IndexError):
+    assert stack.getMin() is None
 
-# Additional tests can be added to cover more scenarios.
+
+def test_min_stack_operations():
+    operations = ["MinStack", "push", "push", "push", "top", "pop", "getMin", "pop", "getMin", "pop", "push", "top", "getMin", "push", "top", "getMin", "pop", "getMin"]
+    inputs = [[], [2147483646], [2147483646], [2147483647], [], [], [], [], [], [], [2147483647], [], [], [-2147483648], [], [], [], []]
+    expected_outputs = [None, None, None, None, 2147483647, None, 2147483646, None, 2147483646, None, None, 2147483647, 2147483647, None, -2147483648, -2147483648, None, 2147483647]
+
+    min_stack = None
+    for op, value, expected in zip(operations, inputs, expected_outputs):
+        if op == "MinStack":
+            min_stack = MinStack()
+        elif op == "push":
+            min_stack.push(value[0])
+        elif op == "pop":
+            min_stack.pop()
+        elif op == "top":
+            top_val = min_stack.top()
+            assert top_val == expected, f"Top value expected to be {expected}, got {top_val}"
+        elif op == "getMin":
+            min_val = min_stack.getMin()
+            assert min_val == expected, f"Min value expected to be {expected}, got {min_val}"
