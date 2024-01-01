d08ff8312c8d6ddc9ea98aeda3bb3ecdc7a84c7d
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2024-01-01 22:39:42 +0100

    merge sort repetition

--- leetcode/sorting_merge_sort_rep1.py
+++ leetcode/sorting_merge_sort_rep1.py
@@ -1,11 +1,14 @@
+
+
+
 def merge_sort(arr):
 
     if len(arr) <= 1:
         return arr
     
     mid = len(arr) // 2
-    left = merge_sort[:mid]
-    right = merge_sort[mid:]
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
 
     return merge(left, right)
 
