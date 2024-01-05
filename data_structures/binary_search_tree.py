7683313f82c5b17cd54f94f165ce3320cbfe3ddb
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2024-01-05 12:30:50 +0100

    binary search tree implementation

---
+++ data_structures/binary_search_tree.py
@@ -0,0 +1,47 @@
+
+
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, val) -> bool:
+        if self.root is None:
+            self.root = Node(val)
+        
+        temp = self.root
+        new_node = Node(val)
+        while True:
+            if temp.value == new_node.value:
+                return False
+            # go left
+            if temp.value > new_node.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right                
+
+    def contains(self, value) -> bool:
+        temp = self.root
+        while temp is not None:
+            if temp.value == value:
+                return True
+            # go left
+            if temp.value > value:
+                temp = temp.left
+            else:
+                temp = temp.right   
+        return False
---
+++ tests/test_datastruc_binary_search_tree.py
@@ -0,0 +1,18 @@
+
+from data_structures.binary_search_tree import BinarySearchTree
+
+def test_insert_and_contains():
+    bst = BinarySearchTree()
+    values = [15, 10, 20, 8, 12, 16, 25]
+
+    for val in values:
+        bst.insert(val)
+
+    for val in values:
+        assert bst.contains(val) is True
+
+    assert bst.contains(100) is False
+
+def test_empty_tree():
+    bst = BinarySearchTree()
+    assert bst.contains(15) is False
