9918eafce4a8956c6dd50bc17bca470173bd8295
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2024-03-24 11:58:28 +0100

    first solution

---
+++ leetcode/problem_973_kClosest.py
@@ -0,0 +1,31 @@
+
+import heapq
+from typing import List
+
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+
+        distance_fun = lambda p: (p[0]*p[0]+ p[1]*p[1])
+        
+        distances = [
+            (distance_fun(p), [p[0], p[1]])
+            for p in points
+        ]
+        # O(n)
+        heapq.heapify(distances)
+        print(distances)
+
+        return [
+            heapq.heappop(distances)[1]
+            for _ in range(k)
+        ]
+
+
+if __name__ == "__main__":
+
+    sol = Solution()
+    points = [[3,3],[5,-1],[-2,4]]
+    k = 2
+    output = sol.kClosest(points, k)
+    assert [[3,3],[-2,4]] == output
