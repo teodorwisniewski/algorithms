ecdf34f233630d40fd9af17673bf2be2f90a77dc
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2024-03-24 12:01:43 +0100

    mroe optimal solution

--- leetcode/problem_973_kClosest.py
+++ leetcode/problem_973_kClosest.py
@@ -2,24 +2,42 @@
 import heapq
 from typing import List
 
+# class Solution:
+#     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+
+#         distance_fun = lambda p: (p[0]*p[0]+ p[1]*p[1])
+        
+#         distances = [
+#             (distance_fun(p), [p[0], p[1]])
+#             for p in points
+#         ]
+#         # O(n)
+#         heapq.heapify(distances)
+#         print(distances)
+
+#         return [
+#             heapq.heappop(distances)[1]
+#             for _ in range(k)
+#         ]
+
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
 
-        distance_fun = lambda p: (p[0]*p[0]+ p[1]*p[1])
+        heap = []
         
-        distances = [
-            (distance_fun(p), [p[0], p[1]])
-            for p in points
-        ]
-        # O(n)
-        heapq.heapify(distances)
-        print(distances)
+        for x, y in points:
+            dist = - (x**2 + y**2)
+            if len(heap) == k:
+                heapq.heappushpop(heap, (dist, x, y))
+            else:
+                heapq.heappush(heap, (dist, x, y))
 
         return [
-            heapq.heappop(distances)[1]
-            for _ in range(k)
+            [x, y]
+            for (dist, x, y) in heap
         ]
+
 
 
 if __name__ == "__main__":
@@ -28,4 +46,4 @@ if __name__ == "__main__":
     points = [[3,3],[5,-1],[-2,4]]
     k = 2
     output = sol.kClosest(points, k)
-    assert [[3,3],[-2,4]] == output
+    assert sorted([[3,3],[-2,4]]) == sorted(output)
